ddfcc0c111728ef6e7353a2de2ecc3cf1387795c
Josephat Macharia <josemash4@gmail.com>
2018-05-27 01:58:35 +0300

    added production settings

--- sito/sito/wsgi.py
+++ sito/sito/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sito.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sito.live_settings")
 
 application = get_wsgi_application()
