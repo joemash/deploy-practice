edc87cd85a72782653147e3a9cb12b9c2a0daa2c
Josephat Macharia <josemash4@gmail.com>
2018-05-27 22:42:41 +0300

    static file refacotring

--- sito/sito/live_settings.py
+++ sito/sito/live_settings.py
@@ -122,14 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = '/webapps/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
-
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
