9ebb1ee6a2849e15b2d4c8eb196da901e4cf2039
Josephat Macharia <josemash4@gmail.com>
2018-05-27 04:14:02 +0300

    changed dbuser

--- sito/sito/live_settings.py
+++ sito/sito/live_settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'trainingdb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'USER': 'mashaa',
+        'PASSWORD': 'mashaa',
         'HOST': '',
         'PORT': '',
     }
